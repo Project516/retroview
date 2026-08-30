62883cf6beefaf3680834e14311f5ec710fdad27
project516 <138796702+Project516@users.noreply.github.com>
2026-08-30 18:22:48 -0500

    port to Minecraft 26.2

--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@
 export SDKMAN_DIR="$HOME/.sdkman"
 [[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]] && source "$SDKMAN_DIR/bin/sdkman-init.sh"
 
-sdk use java 25.0.2-tem
+sdk install java 25.0.3-tem < /dev/null || true
+sdk use java 25.0.3-tem
 
 ./clean.sh
 ./gradlew build runShaderClient
